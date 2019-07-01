fda275f3d55effea06795e8719e404747445a9fb
CompG33k <N.J.Velasco@sbcglobal.net>
2019-07-01 16:30:46 -0700

    Updates

---
+++ precache-manifest.a90ace3784f59a143818db2176e89b09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a96f8a166555b41e7e91b95d6eb6ccb4",
+    "url": "/TheFullStackDeveloper/03299f19ce2795222c86.worker.js"
+  },
+  {
+    "revision": "46d09ab5d640faf2003929949c03637a",
+    "url": "/TheFullStackDeveloper/index.html"
+  },
+  {
+    "revision": "3a0e43c7cd367e593e18",
+    "url": "/TheFullStackDeveloper/static/css/main.4abdc644.chunk.css"
+  },
+  {
+    "revision": "75cd710a89e7fa9e11cd",
+    "url": "/TheFullStackDeveloper/static/js/2.f0d8cdf8.chunk.js"
+  },
+  {
+    "revision": "3a0e43c7cd367e593e18",
+    "url": "/TheFullStackDeveloper/static/js/main.047e24ec.chunk.js"
+  },
+  {
+    "revision": "466e880b092f9ea3e000",
+    "url": "/TheFullStackDeveloper/static/js/runtime~main.6a124bf9.js"
+  },
+  {
+    "revision": "3d0ba7d007f38424a4173e5b98cc5671",
+    "url": "/TheFullStackDeveloper/static/media/GorillaSuit.3d0ba7d0.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/TheFullStackDeveloper/home/precache-manifest.5771e718fa041683cd4d7f2fa6f62197.js"
+  "/TheFullStackDeveloper/precache-manifest.a90ace3784f59a143818db2176e89b09.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TheFullStackDeveloper/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TheFullStackDeveloper/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
